0e4212102d769289b290ae42cdc3b5a9e2af707b
rfearing <rfearing@github.com>
2023-09-08 17:05:42 -0400

    Add existing cards (smushed)

--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
-import { Button, Menu } from '@contentful/f36-components';
+import { Button, Menu, EntryCard } from '@contentful/f36-components';
 import { ChevronDownIcon } from '@contentful/f36-icons';
 import { FieldAppSDK, ContentType, Entry } from '@contentful/app-sdk';
 import { useSDK } from '@contentful/react-apps-toolkit';
@@ -16,6 +17,10 @@ const buttonStyles = css`
 const Field = () => {
   const sdk = useSDK<FieldAppSDK>();
   const locale = sdk.locales.default;
+  const [contentTypes, setContentTypes] = useState<ContentType[]>([]);
+  const [entries, setEntries] = useState<Pick<Entry, 'sys'>[]>([]);
+  const [fullEntries, setFullEntries] =  useState<Entry[]>([]);
+  sdk.window.startAutoResizer();
 
   // linkValidations: Top level validations
   const linkValidations = sdk.field.validations
@@ -26,14 +31,26 @@ const Field = () => {
   const modelRestrictions: string[] = validations.flatMap(validation => validation?.linkContentType ?? []);
 
   // Fetch the content types so that we can display the names, e.g. "Add new {content type name}"
-  const [contentTypes, setContentTypes] = useState<ContentType[]>([]);
   useEffect(() => {
-    const fetchContentTypes = async () => {
+    (async () => {
       const contentTypes = await Promise.all(modelRestrictions.map(id => sdk.cma.contentType.get(({contentTypeId:id}))))
       setContentTypes(contentTypes);
-    };
-    fetchContentTypes();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    })();
+  }, []);
+
+  // Fetch current linked entries
+  useEffect(() => {
+    const previousEntries: Entry[] = sdk.entry.fields[sdk.field.id].getValue() || []
+    setEntries(previousEntries);
+    (async () => {
+      if (previousEntries.length > 0) {
+        const entryObjects = await Promise.all(previousEntries.map(entry => {
+          return sdk.cma.entry.get({entryId: entry.sys.id}) || [];
+        }))
+        setFullEntries(entryObjects);
+      }
+    })();
+  }, []);
 
   // We cannot open a new entry dialog with a dynamic default value 🫤 so we need to create the entry first
   const createEntry = async (contentTypeId: string, key: string, value: Locale) => {
@@ -49,9 +66,8 @@ const Field = () => {
   };
 
   const attachEntry = (entity: Entry<{[x: string]: Locale}>) => {
-    const previous = sdk.entry.fields[sdk.field.id].getValue() || [];
     sdk.entry.fields[sdk.field.id].setValue([
-      ...previous,
+      ...entries,
       {
         sys: {
           type: "Link",
@@ -64,55 +80,78 @@ const Field = () => {
     sdk.navigator.openEntry(entity.sys.id, { slideIn: true });
   };
 
+  const CurrentEntries = () => {
+    return fullEntries.map(entry => {
+      const contentType = contentTypes.find(contentType => contentType.sys.id === entry.sys.contentType.sys.id);
+      if (!contentType) {
+        return null;
+      }
+      return (
+        <EntryCard
+          status="published"
+          contentType={contentType.name}
+          title={entry.fields[contentType.displayField][locale]}
+        />
+      )
+    })
+  }
+
   if (contentTypes.length === 0) {
     return (
-      <Button onClick={() => sdk.dialogs.selectSingleEntry()}>
-        Add existing content
-      </Button>
+      <>
+        <CurrentEntries />
+        <Button onClick={() => sdk.dialogs.selectSingleEntry()}>
+          Add existing content
+        </Button>
+      </>
     )
   }
 
   return (
-    <Menu>
-      <Menu.Trigger>
-        <Button>
-          <span className={buttonStyles}>
-            Add content&nbsp;&nbsp;
-            <ChevronDownIcon variant="secondary" size="tiny" />
-          </span>
-        </Button>
-      </Menu.Trigger>
+    <>
+      <CurrentEntries />
+      <Menu>
+        <Menu.Trigger>
+          <Button>
+            <span className={buttonStyles}>
+              Add content&nbsp;&nbsp;
+              <ChevronDownIcon variant="secondary" size="tiny" />
+            </span>
+          </Button>
+        </Menu.Trigger>
 
-      {/* TODO Show Existing!! */}
-      {/* TODO: Add size validation */}
-      <Menu.List>
-        <Menu.SectionTitle>Add existing content</Menu.SectionTitle>
+        {/* TODO Show Existing!! */}
 
-        {contentTypes.map(contentType => (
-          <Menu.Item onClick={() => sdk.dialogs.selectSingleEntry({contentTypes: [contentType.sys.id]})}>
-            {/* TODO: Save selecting existing */}
+        {/* TODO: Add size validation */}
+        <Menu.List>
+          <Menu.SectionTitle>Add existing content</Menu.SectionTitle>
+
+          {contentTypes.map(contentType => (
+            <Menu.Item onClick={() => sdk.dialogs.selectSingleEntry({contentTypes: [contentType.sys.id]})}>
+              {/* TODO: Save selecting existing */}
+              {contentType.name}
+            </Menu.Item>
+          ))}
+
+          <Menu.Divider />
+          <Menu.SectionTitle>Add new content</Menu.SectionTitle>
+
+          {/* Todo: Add new content and ensure that adding new is allowed */}
+          {contentTypes.map(contentType => (
+            <Menu.Item onClick={() => {
+              // `title` would be "Page Title::Section" if:
+              // - current content type is `Page`
+              // - the display field value is set to "Page Title"
+              // - the linked content type is `Section`
+              const title = `${String(sdk.entry.fields[sdk.contentType.displayField].getValue())}::${contentType.name}`;
+              createEntry(contentType.sys.id, contentType.displayField, { [locale]: title }).then(entity => attachEntry(entity));
+            }}>
             {contentType.name}
-          </Menu.Item>
-        ))}
-
-        <Menu.Divider />
-        <Menu.SectionTitle>Add new content</Menu.SectionTitle>
-
-        {/* Todo: Add new content and ensure that adding new is allowed */}
-        {contentTypes.map(contentType => (
-          <Menu.Item onClick={() => {
-            // `title` would be "Page Title::Section" if:
-            // - current content type is `Page`
-            // - the display field value is set to "Page Title"
-            // - the linked content type is `Section`
-            const title = `${String(sdk.entry.fields[sdk.contentType.displayField].getValue())}::${contentType.name}`;
-            createEntry(contentType.sys.id, contentType.displayField, { [locale]: title }).then(entity => attachEntry(entity));
-          }}>
-           {contentType.name}
-          </Menu.Item>
-        ))}
-      </Menu.List>
-    </Menu>
+            </Menu.Item>
+          ))}
+        </Menu.List>
+      </Menu>
+    </>
   )
 };
 
